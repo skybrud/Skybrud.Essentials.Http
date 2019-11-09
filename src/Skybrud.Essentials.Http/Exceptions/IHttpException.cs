3f6d41cb426ec91285caf754421c0a7ff7e75bd5
Anders Bjerner <abjerner@skybrud.dk>
2019-11-09 13:33:40 +0100

    Introduced new IHttpException interface

---
+++ src/Skybrud.Essentials.Http/Exceptions/IHttpException.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+
+namespace Skybrud.Essentials.Http.Exceptions {
+
+    /// <summary>
+    /// Interface representing an error response as the result of a HTTP request.
+    /// </summary>
+    interface IHttpException {
+
+        /// <summary>
+        /// Gets a reference to the underlying <see cref="IHttpResponse"/>.
+        /// </summary>
+        IHttpResponse Response { get; }
+
+        /// <summary>
+        /// Gets the HTTP status code of the response.
+        /// </summary>
+        HttpStatusCode StatusCode { get; }
+
+    }
+
+}
