bbb1c0582938375d8f512c786f29a1ba6b596654
Anders Bjerner <abjerner@skybrud.dk>
2019-12-04 21:35:49 +0100

    Introduced new interface IHttpRequestOptions

---
+++ src/Skybrud.Essentials.Http/Options/IHttpRequestOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Skybrud.Essentials.Http.Options {
+
+    /// <summary>
+    /// Options for a request.
+    /// </summary>
+    public interface IHttpRequestOptions {
+
+        /// <summary>
+        /// Returns a new <see cref="IHttpRequest"/> instance for this options instance.
+        /// </summary>
+        /// <returns>An instance of <see cref="IHttpRequest"/>.</returns>
+        IHttpRequest GetRequest();
+
+    }
+
+}
