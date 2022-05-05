7e01cfb6e02de6d1b73fea563534a646fe1f0450
Anders Bjerner <abjerner@skybrud.dk>
2022-05-05 19:58:23 +0200

    Introduced new "IHttpClientAsync" interface
    The new interface serves an async counterpart of the existing "IHttpClient" interface, which describes methods for making non-async requests.
    
    This commit also makes the "HttpClient" class implement the new interface so it can be used for both non-async and async requests.
    
    See #3

--- src/Skybrud.Essentials.Http/Client/HttpClient.cs
+++ src/Skybrud.Essentials.Http/Client/HttpClient.cs
@@ -4,13 +4,14 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Skybrud.Essentials.Http.Collections;
 using Skybrud.Essentials.Http.Options;
+using System.Threading.Tasks;
 
 namespace Skybrud.Essentials.Http.Client {
 
     /// <summary>
     /// Class representing a client for making HTTP requests.
     /// </summary>
-    public partial class HttpClient : IHttpClient {
+    public partial class HttpClient : IHttpClient, IHttpClientAsync {
 
         #region DoHttpGetRequest
 
@@ -907,6 +908,30 @@ namespace Skybrud.Essentials.Http.Client {
             if (request == null) throw new ArgumentNullException(nameof(request));
             PrepareHttpRequest(request);
             return request.GetResponse();
+        }
+
+        /// <summary>
+        /// Executes a new the request described by the specified <paramref name="options"/> and returns the
+        /// response as an instance of <see cref="IHttpResponse"/>.
+        /// </summary>
+        /// <returns>An instance of <see cref="Task{IHttpResponse}"/> representing the response.</returns>
+        public async Task<IHttpResponse> GetResponseAsync(IHttpRequestOptions options) {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+            IHttpRequest request = options.GetRequest();
+            PrepareHttpRequest(request);
+            return await request.GetResponseAsync();
+        }
+
+        /// <summary>
+        /// Executes the specified <paramref name="request"/> and returns the response as an instance of
+        /// <see cref="IHttpResponse"/>.
+        /// </summary>
+        /// <param name="request">An instance of <see cref="IHttpRequest"/> describing the request.</param>
+        /// <returns>An instance of <see cref="Task{IHttpResponse}"/> representing the raw response.</returns>
+        public virtual async Task<IHttpResponse> GetResponseAsync(IHttpRequest request) {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            PrepareHttpRequest(request);
+            return await request.GetResponseAsync();
         }
 
         /// <summary>
