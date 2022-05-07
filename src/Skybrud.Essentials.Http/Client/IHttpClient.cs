426239989a9a82d0349f8fe6ab65d21b482694d3
Anders Bjerner <abjerner@skybrud.dk>
2022-05-07 13:36:43 +0200

    Isolating "GetResponse" methods out into a new base interface
    The new "IHttpClient2" interface now only describes the two "GetResponse" methods, and the "IHttpClient" interface describes all the "DoHttp{Verb}Request" methods that are now marked as obsolete.
    
    For a future release (likely v2), the "IHttpClient" interface will be removed, and the "IHttpClient2" interface will be renamed to "IHttpClient".

--- src/Skybrud.Essentials.Http/Client/IHttpClient.cs
+++ src/Skybrud.Essentials.Http/Client/IHttpClient.cs
@@ -10,21 +10,7 @@ namespace Skybrud.Essentials.Http.Client {
     /// <summary>
     /// Interface describing a client for making HTTP requests.
     /// </summary>
-    public interface IHttpClient {
-
-        /// <summary>
-        /// Returns the response of the request identified by the specified <paramref name="options"/>.
-        /// </summary>
-        /// <param name="options">The options for the request to the API.</param>
-        /// <returns>An instance of <see cref="IHttpResponse"/> representing the raw response.</returns>
-        IHttpResponse GetResponse(IHttpRequestOptions options);
-
-        /// <summary>
-        /// Sends a new request as described by <paramref name="request"/> and returns the response.
-        /// </summary>
-        /// <param name="request">An instance of <see cref="IHttpRequest"/> describing the request.</param>
-        /// <returns>An instance of <see cref="IHttpResponse"/> representing the raw response.</returns>
-        IHttpResponse GetResponse(IHttpRequest request);
+    public interface IHttpClient : IHttpClient2 {
 
         #region DoHttpGetRequest
 
