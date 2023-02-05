84dadf809a03a05b4b048f427edee01d4b5520bd
Anders Bjerner <abjerner@skybrud.dk>
2023-02-05 15:44:32 +0100

    Introduced new "HttpException" class
    The new class provides a concrete implementation of the "IHttpException" interface.

---
+++ src/Skybrud.Essentials.Http/Exceptions/HttpException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+
+namespace Skybrud.Essentials.Http.Exceptions {
+
+    /// <summary>
+    /// Class representing a basic HTTP exception.
+    /// </summary>
+    public class HttpException : Exception, IHttpException {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets a reference to the underlying <see cref="IHttpResponse"/>.
+        /// </summary>
+        public IHttpResponse Response { get; }
+
+        /// <summary>
+        /// Gets the HTTP status code returned by the Toggl API.
+        /// </summary>
+        public HttpStatusCode StatusCode => Response.StatusCode;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new exception based on the specified <paramref name="response"/>.
+        /// </summary>
+        /// <param name="response">The instance of <see cref="IHttpResponse"/> representing the raw response.</param>
+        public HttpException(IHttpResponse response) : base($"Invalid response received (status: {(int) response.StatusCode})") {
+            Response = response;
+        }
+
+        #endregion
+
+    }
+
+}
